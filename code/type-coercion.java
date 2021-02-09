297c89126f8c255d67b046cac01b4e8f42bc7c3f
Oliver Fobian <oliver.fobian@check24.de>
2021-02-09 12:16:18 +0100

    Added type-coercion.java and type-coercion.kt contents.

---
+++ code/type-coercion.java
@@ -0,0 +1,5 @@
+String label = "The width is ";
+int width = 94;
+String widthLabel = label + width;
+System.out.println(widthLabel);
+// widthLabel is a String
--- code/type-coercion.kt
+++ code/type-coercion.kt
@@ -1,3 +1,7 @@
 val label = "The width is "
 val width = 94
 val widthLabel = label + width
+if(widthLabel is String) {
+        println("widthLabel is of type String")
+    }
+// widthLabel is a String
