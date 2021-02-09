1bcc31f865c2745030a3ef2c330b7479ca3f2b0c
Oliver Fobian <oliver.fobian@check24.de>
2021-02-09 13:30:52 +0100

    Added string-interpolation.java and string-interpolation.kt contents.

---
+++ code/string-interpolation.java
@@ -0,0 +1,4 @@
+int apples = 3;
+int oranges = 5;
+String fruitSummary = "I have " + (apples + oranges) +
+    " pieces of fruit.";
--- code/string-interpolation.kt
+++ code/string-interpolation.kt
@@ -1,4 +1,7 @@
 val apples = 3
 val oranges = 5
-val fruitSummary = "I have ${apples + oranges} " +
-                   "pieces of fruit."
+val fruitSummary = "I have ${apples + oranges}" +
+    " pieces of fruit."
+
+// String Interpolation:
+// Evaluate a string literal that contains one or more placeholders.
