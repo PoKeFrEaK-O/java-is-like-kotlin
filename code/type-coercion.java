5355aa04a6d3a9d5f09d3d2632cfa84108aa67c4
Oliver Fobian <oliver.fobian@check24.de>
2021-02-11 17:38:03 +0100

    updated TypeCoercion.java and TypeCoercion.kt

--- code/type-coercion.java
+++ code/type-coercion.java
@@ -3,3 +3,11 @@ int width = 94;
 String widthLabel = label + width;
 System.out.println(widthLabel);
 // widthLabel is a String
+
+// TypeCoercion Error Example:
+System.out.println('A' + 0);
+// 65
+System.out.println('A' + 50 + "A");
+// 115A
+System.out.println("A" + 'A' + 50);
+// AA50
--- code/type-coercion.kt
+++ code/type-coercion.kt
@@ -5,3 +5,11 @@ if(widthLabel is String) {
         println("widthLabel is of type String")
     }
 // widthLabel is a String
+
+// TypeCoercion Error Example:
+println('A' + 0)
+// A
+println('A' + 50 + "A")
+// sA
+println("A" + 'A' + 50)
+// AA50
