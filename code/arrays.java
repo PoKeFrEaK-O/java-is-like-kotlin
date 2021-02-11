2506bb5d4750a15ad4b2e0ef734650baba2d3a55
Oliver Fobian <oliver.fobian@check24.de>
2021-02-11 14:16:09 +0100

    Added arrays.java and arrays.kt content.

--- code/arrays.java
+++ code/arrays.java
@@ -1,2 +1,9 @@
-List<String> shoppingList = Arrays.asList("catfish", "water", "tulips", "blue paint");
-        shoppingList.set(1, "bottle of water");
+List<String> shoppingList = java.util.Arrays.asList("catfish", "water", "tulips", "blue paint");
+shoppingList.set(1, "bottle of water");
+for (String s : shoppingList) {
+    System.out.println(s);
+}
+// catfish
+// bottle of water
+// tulips
+// blue paint
--- code/arrays.kt
+++ code/arrays.kt
@@ -1,3 +1,10 @@
 val shoppingList = arrayOf("catfish", "water",
         "tulips", "blue paint")
-shoppingList[1] = "bottle of water"
+    shoppingList[1] = "bottle of water"
+    for (element in shoppingList) {
+        println(element)
+    }
+// catfish
+// bottle of water
+// tulips
+// blue paint
