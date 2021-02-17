f16e5edbb78bd717ba6e385eb0e7b146a8511543
Oliver Fobian <oliver.fobian@check24.de>
2021-02-17 17:12:54 +0100

    Added for.kt content.

---
+++ code/for.kt
@@ -0,0 +1,14 @@
+for (counter in 1..5) {
+    println("Zaeler ist bei: $counter")
+}
+// Ausgabe:
+// Zaeler ist bei: 1
+// Zaeler ist bei: 2
+// Zaeler ist bei: 3
+// Zaeler ist bei: 4
+// Zaeler ist bei: 5
+
+// No need to declare the data type of variable.
+// If iterating over a range, we can use in
+// the lower and upper (including) limit can
+// be defined on both the sides of .. Operator.
