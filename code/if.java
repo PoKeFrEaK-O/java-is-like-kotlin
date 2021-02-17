59d503389f4872edc5bfdca02b571a7934a0af70
Oliver Fobian <oliver.fobian@check24.de>
2021-02-17 18:11:26 +0100

    Added if.java and if.kt content.

---
+++ code/if.java
@@ -0,0 +1,10 @@
+int zahl = 2;
+if (zahl <= 1){
+    System.out.println(zahl + " kleiner oder gleich 1");
+} else if (zahl == 2) {
+    System.out.println(zahl + " ist gleich 2");
+} else {
+    System.out.println(zahl + " ist größer als 2");
+}
+// Ausgabe:
+// 2 ist gleich 2
---
+++ code/if.kt
@@ -0,0 +1,10 @@
+var zahl: Int = 2
+if (zahl <= 1) {
+    println("$zahl kleiner oder gleich 1")
+} else if (zahl == 2) {
+    println("$zahl ist gleich 2")
+} else {
+    println("$zahl ist größer als 2")
+}
+// Ausgabe:
+// 2 ist gleich 2
