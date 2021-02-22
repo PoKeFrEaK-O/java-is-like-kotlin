2d8e73529df1c52526effc317a5a6bfa1b335d92
Oliver Fobian <oliver.fobian@check24.de>
2021-02-22 14:19:43 +0100

    Added ArraysKotlin.kt, ArrayKotlin2.kt, ArrayJava.java and ArrayJava2 content.

--- code/arrays.kt
+++ code/arrays.kt
@@ -1,9 +1,39 @@
-val shoppingList = arrayOf("catfish", "water",
-        "tulips", "blue paint")
-    shoppingList[1] = "bottle of water"
-    for (element in shoppingList) {
-        println(element)
-    }
+// declares an array of String:
+
+// allocates memory for 4 String:
+val anArrayWithTheNameArray: Array<String?> = arrayOfNulls(4)
+// ? --> Can be null.
+
+// initialize of elements:
+anArrayWithTheNameArray[0] = "catfish"
+anArrayWithTheNameArray[1] = "water"
+anArrayWithTheNameArray[2] = "tulips"
+anArrayWithTheNameArray[3] = "blue paint"
+
+// Replacing the second element
+anArrayWithTheNameArray[1] = "bottle of water"
+
+for (element in anArrayWithTheNameArray) {
+    println(element)
+}
+// Ausgabe:
+// catfish
+// bottle of water
+// tulips
+// blue paint
+---------------------------------------------------------------
+
+// Declares an array of String and initialize the elements in one:
+val anArrayWithTheNameArray = arrayOf("catfish", "water",
+    "tulips", "blue paint")
+
+// Replacing the second element
+anArrayWithTheNameArray[1] = "bottle of water"
+
+for (element in anArrayWithTheNameArray) {
+    println(element)
+}
+// Ausgabe:
 // catfish
 // bottle of water
 // tulips
