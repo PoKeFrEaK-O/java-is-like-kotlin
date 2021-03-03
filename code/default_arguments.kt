d2f64fa4c0f15d554e12feff9e24031b45509164
Oliver Fobian <Oliver.Fobian@check24.de>
2021-03-03 13:19:18 +0100

    Updated default_arguments.kt and parameters.kt

--- code/default_arguments.kt
+++ code/default_arguments.kt
@@ -1,21 +1,19 @@
 fun main() {
-    var result = product(zahl = 5) // Default argument multiplikator is used.
+    var result = product(number = 5) // Default argument multiplier is used.
     println("Das Ergebnis lautet: $result")
-    result = product(5,3) // Default argument multiplikator is not used.
+    result = product(5,3) // Default argument multiplier is not used.
     println("Das Ergebnis lautet: $result")
 }
 
 // Function parameters can have default values,
 // which are used when you skip the corresponding argument:
 fun product(
-        zahl: Int,
-        multiplikator: Int = 5,
+        number: Int,
+        multiplier: Int = 5, // This is a default value
 ): Int
 {
-    return zahl * multiplikator
+    return number * multiplier
 }
-//multiplikator is a default value.
-
 // Ausgabe:
 // Das Ergebnis lautet: 25
 // Das Ergebnis lautet: 15
--- code/parameters.kt
+++ code/parameters.kt
@@ -1,27 +1,27 @@
 fun main() {
     // Code that is executed
-    var result = parameter(5,5)
+    var result = parameter(20,5)
     println("Das Ergebnis lautet: $result")
-    result = parameterWithLinebreaks(5,5)
+    result = parameterWithLinebreaks(20,5)
     println("Das Ergebnis lautet: $result")
 }
 
 // Definition of parameters: name: type
 // Parameters are separated using commas.
-fun parameter(zahl: Int, multiplikator: Int): Int {
-    return zahl * multiplikator
+fun parameter(number: Int, subtracted: Int): Int {
+    return number - subtracted
 }
 
 // Function with line breaks between parameters:
 fun parameterWithLinebreaks(
-        zahl: Int,
-        multiplikator: Int, // trailing comma
+        number: Int,
+        subtracted: Int, // trailing comma
 ): Int // Type that is returned
 {
-    return zahl * multiplikator
-    // The return keyword is used to return the product of the val zahl and multiplikator.
+    return number - subtracted
+    // The return keyword is used to return the difference of the val number and returned.
 
 }
 // Ausgabe:
-// Das Ergebnis lautet: 25
-// Das Ergebnis lautet: 25
+// Das Ergebnis lautet: 15
+// Das Ergebnis lautet: 15
