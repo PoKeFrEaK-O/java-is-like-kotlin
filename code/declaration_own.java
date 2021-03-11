61e266cf0ad2902882c771ec156229971b8e87f7
Oliver Fobian <Oliver.Fobian@check24.de>
2021-03-11 14:54:15 +0100

    Added a Declaration.java class.

---
+++ code/declaration_own.java
@@ -0,0 +1,16 @@
+public class Declaration {
+    // Classes in Java are declared using the keyword class:
+    public class Classname {
+        // Code of the class Classname
+    }
+
+    // Curly braces cannot be omitted even if the class has no body:
+    public class Empty {}
+
+    // Declaring a class with multiple properties:
+    public class Person{
+        String firstName; // Create a class attribute
+        String lastName;
+        int age;
+    }
+}
